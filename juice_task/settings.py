824e12ff8f0d396392d60a46071c2bccd119fa27
freshHarvestHyd <freshharvest.hyd@gmail.com>
2025-04-21 01:21:08 +0530

    Settings changes

--- juice_task/settings.py
+++ juice_task/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,14 +77,11 @@ WSGI_APPLICATION = "juice_task.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "juice_db",
-        "USER": "postgres",
-        "PASSWORD": "your_password",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:your_local_password@localhost:5432/juice_db',
+        conn_max_age=600,
+        ssl_require=not DEBUG  # Require SSL in production
+    )
 }
 
 
