cadb632921f7ab401383081f2421f0f8e5892eda
Amruth22 <89969774+Amruth22@users.noreply.github.com>
2025-05-03 13:53:29 +0530

    Update settings.py to use MySQL instead of PostgreSQL

--- juice_task/settings.py
+++ juice_task/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url  # Commented out as we're not using dj_database_url for MySQL
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,12 +76,20 @@ WSGI_APPLICATION = "juice_task.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Updated to use MySQL instead of PostgreSQL
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://juice_db_drbd_user:XrWKZI7n0vehFTaQ22PTYYo3krPrOE7h@dpg-d02kg93e5dus73bt74ng-a.oregon-postgres.render.com/juice_db_drbd',
-        conn_max_age=600,
-        ssl_require=not DEBUG  # Require SSL in production
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'juice_task_db',  # Your MySQL database name
+        'USER': 'root',           # Your MySQL username
+        'PASSWORD': '',           # Your MySQL password (empty for development)
+        'HOST': 'localhost',      # MySQL server address
+        'PORT': '3306',           # MySQL default port
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
 }
 
 
